9de29c2edb76c00eb266d2ce897350401a218028
Jin Kwon <onacit@gmail.com>
2019-07-15 10:44:58 +0900

    updated

--- sakila-db.run.sh
+++ sakila-db.run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo trying to stop already running container...
-sh ./docker.stop.sh
+sh ./sakila-db.stop.sh
 echo running container...
 docker run --rm -d --name sakila-db -p 33306:3306 jinahya/sakila-db
