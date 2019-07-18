4896b874a624b9a0937138233b65ccb0742f66b1
Jin Kwon <onacit@gmail.com>
2019-07-18 10:04:06 +0900

    updated

--- src/test/java/com/github/jinahya/sakila/persistence/ActorService.java
+++ src/test/java/com/github/jinahya/sakila/persistence/ActorService.java
@@ -20,7 +20,7 @@ package com.github.jinahya.sakila.persistence;
  * #L%
  */
 
-import java.util.stream.Stream;
+import java.util.List;
 
 /**
  * A service class for {@link Actor}.
@@ -41,36 +41,36 @@ class ActorService extends BaseEntityService<Actor> {
     // -----------------------------------------------------------------------------------------------------------------
 
     /**
-     * Returns a stream of actors, whose {@link FullName#ATTRIBUTE_NAME_LAST_NAME} attribute <i>optionally</i> matches
-     * to specified value, ordered by {@link FullName#ATTRIBUTE_NAME_FIRST_NAME} in either ascending or descending
-     * indicated by specified flag.
+     * Returns a list of actors, which each {@link FullName#ATTRIBUTE_NAME_LAST_NAME} attribute <i>optionally</i>
+     * matches to specified value, ordered by {@link FullName#ATTRIBUTE_NAME_FIRST_NAME} in either ascending or
+     * descending indicated by specified flag.
      *
      * @param lastName       a value for {@link FullName#ATTRIBUTE_NAME_LAST_NAME} to <i>optionally</i> match; may be
      *                       {@code null}.
      * @param ascendingOrder {@code true} for ascending order; {@code false} for descending order.
      * @param firstResult    a value for {@link javax.persistence.TypedQuery#setFirstResult(int)}
      * @param maxResults     a value for {@link javax.persistence.TypedQuery#setMaxResults(int)}
-     * @return a stream of, optionally matched, actors.
+     * @return a list of, optionally matched, actors.
      */
-    public Stream<Actor> streamOrderedByFirstName(final String lastName, final boolean ascendingOrder,
-                                                  final Integer firstResult, final Integer maxResults) {
+    public List<Actor> listSortedByFirstName(final String lastName, final boolean ascendingOrder,
+                                             final Integer firstResult, final Integer maxResults) {
         throw new UnsupportedOperationException("not implemented yet");
     }
 
     /**
-     * Returns a stream of actors, whose {@link FullName#ATTRIBUTE_NAME_FIRST_NAME} attribute <i>optionally</i> matches
-     * to specified value, ordered by {@link FullName#ATTRIBUTE_NAME_LAST_NAME} in either ascending or descending
-     * indicated by specified flag.
+     * Returns a list of actors, which each {@link FullName#ATTRIBUTE_NAME_FIRST_NAME} attribute <i>optionally</i>
+     * matches to specified value, ordered by {@link FullName#ATTRIBUTE_NAME_LAST_NAME} in either ascending or
+     * descending indicated by specified flag.
      *
      * @param firstName      a value for {@link FullName#ATTRIBUTE_NAME_LAST_NAME} to <i>optionally</i> match; may be
      *                       {@code null}.
      * @param ascendingOrder {@code true} for ascending order; {@code false} for descending order.
      * @param firstResult    a value for {@link javax.persistence.TypedQuery#setFirstResult(int)}
      * @param maxResults     a value for {@link javax.persistence.TypedQuery#setMaxResults(int)}
-     * @return a stream of, optionally matched, actors.
+     * @return a list of, optionally matched, actors.
      */
-    public Stream<Actor> streamOrderedByLastName(final String firstName, final boolean ascendingOrder,
-                                                 final Integer firstResult, final Integer maxResults) {
+    public List<Actor> listSortedByLastName(final String firstName, final boolean ascendingOrder,
+                                            final Integer firstResult, final Integer maxResults) {
         throw new UnsupportedOperationException("not implemented yet");
     }
 }
