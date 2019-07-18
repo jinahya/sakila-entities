25e001a5f3499056e254a52b8cbd1b34031eb60f
Jin Kwon <onacit@gmail.com>
2019-07-18 14:35:58 +0900

    updated

--- src/test/java/com/github/jinahya/sakila/persistence/CategoryService.java
+++ src/test/java/com/github/jinahya/sakila/persistence/CategoryService.java
@@ -23,8 +23,8 @@ package com.github.jinahya.sakila.persistence;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 /**
  * A service for {@link Category}.
@@ -45,28 +45,28 @@ class CategoryService extends BaseEntityService<Category> {
     // -----------------------------------------------------------------------------------------------------------------
 
     /**
-     * Returns a stream of categorys ordered by {@link Category#ATTRIBUTE_NAME_NAME} attribute in the order indicated by
-     * specified flag.
+     * Finds the category whose {@link Category#ATTRIBUTE_NAME_NAME} attribute matches to specified value.
      *
-     * @param ascendingOrder the flag for ordering; {@code true} for ascending order; {@code false} for descending
-     *                       order.
-     * @param firstResult    an optional value for {@link javax.persistence.TypedQuery#setFirstResult(int)}
-     * @param maxResults     an optional value for {@link javax.persistence.TypedQuery#setMaxResults(int)}
-     * @return a stream of, optionally paged, categorys.
+     * @param name the value for {@link Category#ATTRIBUTE_NAME_NAME} attribute to match.
+     * @return an optional of found entity.
      */
-    public Stream<Category> streamOrderedByName(final boolean ascendingOrder, @PositiveOrZero final Integer firstResult,
-                                                @Positive final Integer maxResults) {
+    public Optional<Category> findByName(@NotNull final String name) {
         // TODO: 2019-07-16 implement!!!
         throw new UnsupportedOperationException("not implemented yet");
     }
 
     /**
-     * Finds the category whose {@link Category#ATTRIBUTE_NAME_NAME} attribute matches to specified value.
+     * Returns a list of categories sorted by {@link Category#ATTRIBUTE_NAME_NAME} attribute in the order indicated by
+     * specified flag.
      *
-     * @param name the value of {@link Category#ATTRIBUTE_NAME_NAME} attribute to match.
-     * @return an optional of found entity.
+     * @param ascendingOrder the flag for ordering; {@code true} for ascending order; {@code false} for descending
+     *                       order.
+     * @param firstResult    the position of the first result to retrieve.
+     * @param maxResults     the maximum number of results to retrieve.
+     * @return a stream of, optionally paged, categorys.
      */
-    public Optional<Category> findByName(@NotNull final String name) {
+    public List<Category> listSortedByName(final boolean ascendingOrder, @PositiveOrZero final Integer firstResult,
+                                           @Positive final Integer maxResults) {
         // TODO: 2019-07-16 implement!!!
         throw new UnsupportedOperationException("not implemented yet");
     }
--- src/test/java/com/github/jinahya/sakila/persistence/CategoryServiceIT.java
+++ src/test/java/com/github/jinahya/sakila/persistence/CategoryServiceIT.java
@@ -40,7 +40,6 @@ import static com.github.jinahya.sakila.persistence.Category.comparingName;
 import static java.util.Collections.unmodifiableSortedSet;
 import static java.util.Optional.ofNullable;
 import static java.util.concurrent.ThreadLocalRandom.current;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -97,26 +96,26 @@ class CategoryServiceIT extends BaseEntityServiceIT<CategoryService, Category> {
     // -----------------------------------------------------------------------------------------------------------------
 
     /**
-     * Provides arguments for {@link #testStreamOrderedByName(boolean, Integer, Integer)} method.
-     *
-     * @return a stream of arguments.
-     */
-    private static Stream<Arguments> argumentsForTestingStreamOrderedByName() {
-        return range(8, 17).mapToObj(i -> arguments(
-                current().nextBoolean(), firstResult(Category.class), maxResults(Category.class)));
-    }
-
-    /**
      * Provides arguments for {@link #testFindByName(String)} method.
      *
      * @return a stream of arguments.
      */
-    private static Stream<Arguments> argumentsForTestingFindByName() {
+    private static Stream<Arguments> arguments_testFindByName() {
         return range(8, 17).mapToObj(i -> arguments(
                 NAMES.stream()
                         .skip(current().nextLong(NAMES.size()))
                         .findFirst()
                         .orElseThrow(() -> new RuntimeException("fix me if you see me"))));
+    }
+
+    /**
+     * Provides arguments for {@link #testListSortedByName(boolean, Integer, Integer)} method.
+     *
+     * @return a stream of arguments.
+     */
+    private static Stream<Arguments> arguments_testListSortedByName() {
+        return range(8, 17).mapToObj(i -> arguments(
+                current().nextBoolean(), firstResult(Category.class), maxResults(Category.class)));
     }
 
     // -----------------------------------------------------------------------------------------------------------------
@@ -128,40 +127,15 @@ class CategoryServiceIT extends BaseEntityServiceIT<CategoryService, Category> {
         super(CategoryService.class, Category.class);
     }
 
-    // -----------------------------------------------------------------------------------------------------------------
-
-    /**
-     * Tests {@link CategoryService#streamOrderedByName(boolean, Integer, Integer)} method.
-     *
-     * @param ascendingOrder a value for {@code ascendingOrder} parameter.
-     * @param firstResult    a value for {@code firstResult} parameter.
-     * @param maxResults     a value for {@code maxResults} parameter.
-     */
-    // TODO: 2019-07-16 enable, assert fails, implement, assert passes.
-    @Disabled
-    @MethodSource({"argumentsForTestingStreamOrderedByName"})
-    @ParameterizedTest
-    void testStreamOrderedByName(final boolean ascendingOrder, @PositiveOrZero final Integer firstResult,
-                                 @Positive final Integer maxResults) {
-        final List<Category> list = serviceInstance()
-                .streamOrderedByName(ascendingOrder, firstResult, maxResults)
-                .collect(toList());
-        assertThat(list)
-                .isNotNull()
-                .isSortedAccordingTo(comparingName(ascendingOrder))
-                .hasSizeLessThan(ofNullable(maxResults).orElse(Integer.MAX_VALUE))
-        ;
-    }
-
     // ------------------------------------------------------------------------------------------------------ findByName
 
     /**
-     * Asserts {@link CategoryService#findByName(String)} returns empty for an unknown category.
+     * Asserts {@link CategoryService#findByName(String)} returns an empty for an unknown category.
      */
-    // TODO: 2019-07-16 enable, assert fails, implement, assert passes.
+    // TODO: 2019-07-16 enable, assert fails, implement, and assert passes.
     @Disabled
     @Test
-    void assertFindByNameReturnsEmptyIfNameIsUnknown() {
+    void assertFindByNameReturnsEmptyForUnknown() {
         final String name = "周星馳";
         assertThat(NAMES).doesNotContain(name);
         assertThat(serviceInstance().findByName(name)).isEmpty();
@@ -172,14 +146,38 @@ class CategoryServiceIT extends BaseEntityServiceIT<CategoryService, Category> {
      *
      * @param name a value for {@code name} parameter.
      */
-    // TODO: 2019-07-16 enable, assert fails, implement, assert passes.
+    // TODO: 2019-07-16 enable, assert fails, implement, and assert passes.
     @Disabled
-    @MethodSource({"argumentsForTestingFindByName"})
+    @MethodSource({"arguments_testFindByName"})
     @ParameterizedTest
     void testFindByName(@NotNull final String name) {
         assertThat(serviceInstance().findByName(name))
                 .isPresent()
-                .hasValueSatisfying(category -> assertThat(category.getName()).isEqualTo(name))
+                .hasValueSatisfying(c -> assertThat(c.getName()).isEqualTo(name))
+        ;
+    }
+
+    // ------------------------------------------------------------------------------------------------ listSortedByName
+
+    /**
+     * Tests {@link CategoryService#listSortedByName(boolean, Integer, Integer)} method.
+     *
+     * @param ascendingOrder a value for {@code ascendingOrder} parameter.
+     * @param firstResult    a value for {@code firstResult} parameter.
+     * @param maxResults     a value for {@code maxResults} parameter.
+     */
+    // TODO: 2019-07-16 enable, assert fails, implement, and assert passes.
+    @Disabled
+    @MethodSource({"arguments_testListSortedByName"})
+    @ParameterizedTest
+    void testListSortedByName(final boolean ascendingOrder, @PositiveOrZero final Integer firstResult,
+                              @Positive final Integer maxResults) {
+        final List<Category> list = serviceInstance().listSortedByName(ascendingOrder, firstResult, maxResults);
+        assertThat(list)
+                .isNotNull()
+                .isSortedAccordingTo(comparingName(ascendingOrder))
+                .size()
+                .satisfies(s -> ofNullable(maxResults).ifPresent(v -> assertThat(s).isLessThanOrEqualTo(v)))
         ;
     }
 }
