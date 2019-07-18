6661b6a63b2c04754cefd230a528735fbb49e991
Jin Kwon <onacit@gmail.com>
2019-07-18 15:10:04 +0900

    updated

--- src/main/java/com/github/jinahya/sakila/persistence/FullNamedBaseEntity.java
+++ src/main/java/com/github/jinahya/sakila/persistence/FullNamedBaseEntity.java
@@ -26,8 +26,11 @@ import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import static com.github.jinahya.sakila.persistence.FullNamedEntity.COLUMN_NAME_FIRST_NAME;
+import static com.github.jinahya.sakila.persistence.FullNamedEntity.COLUMN_NAME_LAST_NAME;
+
 @MappedSuperclass
-abstract class FullNamedBaseEntity extends BaseEntity implements FullNamedEntity {
+abstract class FullNamedBaseEntity extends BaseEntity implements FullNamed {
 
     // -----------------------------------------------------------------------------------------------------------------
 
