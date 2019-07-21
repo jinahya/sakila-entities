f8dc7859f7582fd32b04992081a1b5b686f7a1d4
Jin Kwon <onacit@gmail.com>
2019-07-21 17:53:16 +0900

    updated

--- src/test/sql/film.sql
+++ src/test/sql/film.sql
@@ -1,3 +1,11 @@
 -- --------------------------------------------------------------------------------------------- select special_features
 SELECT special_features
 FROM film;
+
+
+SELECT f.film_id, fc.category_id
+FROM film AS f
+         LEFT OUTER JOIN film_category AS fc ON f.film_id = fc.film_id OR fc.film_id IS NULL
+-- WHERE fc.category_id IS NULL
+ORDER BY f.film_id
+;
