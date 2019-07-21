0d9811b0bcda367d67c7c4f08265173c6fcbc8e8
Jin Kwon <onacit@gmail.com>
2019-07-21 19:36:03 +0900

    updated

--- src/test/java/com/github/jinahya/sakila/persistence/StaffTest.java
+++ src/test/java/com/github/jinahya/sakila/persistence/StaffTest.java
@@ -1,10 +1,13 @@
 package com.github.jinahya.sakila.persistence;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * A class for testing {@link Staff} class.
  *
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
  */
+@Slf4j
 class StaffTest extends BaseEntityTest<Staff> {
 
     // -----------------------------------------------------------------------------------------------------------------
