b6f422364bd3c4c35cd73ad55f00d2cadb4e2cdd
Jin Kwon <onacit@gmail.com>
2019-07-26 17:21:50 +0900

    updated

--- src/test/sql/country.sql
+++ src/test/sql/country.sql
@@ -35,3 +35,11 @@ FROM country AS l
 GROUP BY l.country_id
 ORDER BY city_count DESC
 ;
+
+SELECT l.country_id, COUNT(r.city_id) AS city_count
+FROM country AS l
+         LEFT OUTER JOIN city AS r ON l.country_id = r.country_id
+GROUP BY l.country_id
+ORDER BY city_count DESC
+;
+
