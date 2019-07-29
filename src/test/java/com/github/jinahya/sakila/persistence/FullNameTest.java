ff2fd81593d63dac7981cc8701d67effb390636a
Jin Kwon <onacit@gmail.com>
2019-07-29 11:52:33 +0900

    updated

--- src/test/java/com/github/jinahya/sakila/persistence/FullNameTest.java
+++ src/test/java/com/github/jinahya/sakila/persistence/FullNameTest.java
@@ -20,44 +20,9 @@ package com.github.jinahya.sakila.persistence;
  * #L%
  */
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
  * A class for testing {@link FullName}.
  */
 class FullNameTest {
 
-    // -----------------------------------------------------------------------------------------------------------------
-
-    /**
-     * Tests {@link FullName#getAsFirstNameFirst()}.
-     */
-    @Test
-    void testGetAsFirstNameFirst() {
-        final FullName instance = new FullName();
-        final String firstName = "firstName";
-        final String lastName = "lastName";
-        instance.setFirstName(firstName);
-        instance.setLastName(lastName);
-        final String expected = firstName + " " + lastName;
-        final String actual = instance.getAsFirstNameFirst();
-        assertEquals(expected, actual);
-    }
-
-    /**
-     * Tests {@link FullName#getAsLastNameFirst()}.
-     */
-    @Test
-    void testGetAsLastNameFirst() {
-        final FullName instance = new FullName();
-        final String firstName = "firstName";
-        final String lastName = "lastName";
-        instance.setFirstName(firstName);
-        instance.setLastName(lastName);
-        final String expected = lastName + ", " + firstName;
-        final String actual = instance.getAsLastNameFirst();
-        assertEquals(expected, actual);
-    }
 }
