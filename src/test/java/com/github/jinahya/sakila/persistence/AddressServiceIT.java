37a362d26b29c391f4946db85bd592faf03c2a40
Jin Kwon <onacit@gmail.com>
2019-08-03 17:09:55 +0900

    updated

--- src/test/java/com/github/jinahya/sakila/persistence/AddressServiceIT.java
+++ src/test/java/com/github/jinahya/sakila/persistence/AddressServiceIT.java
@@ -1,12 +1,18 @@
 package com.github.jinahya.sakila.persistence;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Collections.unmodifiableMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * A class for integration-testing {@link AddressService} class.
@@ -42,9 +48,47 @@ class AddressServiceIT extends BaseEntityServiceIT<AddressService, Address> {
     // -----------------------------------------------------------------------------------------------------------------
 
     /**
+     * Provides arguments for {@link #testCount(City, int)} method.
+     *
+     * @return a stream of arguments
+     */
+    private static Stream<Arguments> argumentForTestCount() {
+        return IntStream.range(0, 8)
+                .mapToObj(i -> CITY_ID_ADDRESS_CONT.entrySet()
+                        .stream()
+                        .skip(i)
+                        .findFirst()
+                        .map(e -> {
+                            final int cityId = e.getKey();
+                            final int addressCount = e.getValue();
+                            final City city = BaseEntityServiceIT.findById(City.class, cityId)
+                                    .orElseThrow(() -> new RuntimeException("city not found for " + cityId));
+                            return Arguments.of(city, addressCount);
+                        })
+                        .orElseThrow(() -> new RuntimeException("invalid index"))
+                );
+    }
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
      * Creates a new instance.
      */
     AddressServiceIT() {
         super(AddressService.class, Address.class);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Tests {@link AddressService#count(City)} method.
+     *
+     * @param city     a value for {@code city} argument.
+     * @param expected an expected result of the method.
+     */
+    @MethodSource({"argumentForTestCount"})
+    @ParameterizedTest
+    void testCount(final City city, final int expected) {
+        final long actual = serviceInstance().count(city);
+        assertEquals(expected, actual);
     }
 }
