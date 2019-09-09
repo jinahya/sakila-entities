2bd4a200cf6561d9384ac667f3943dc1f5b26b44
Jin Kwon <onacit@wemakeprice.com>
2019-09-09 18:06:45 +0900

    Update javadoc

--- src/test/java/com/github/jinahya/sakila/persistence/CountryServiceIT.java
+++ src/test/java/com/github/jinahya/sakila/persistence/CountryServiceIT.java
@@ -163,7 +163,7 @@ class CountryServiceIT extends BaseEntityServiceIT<CountryService, Country> {
         super(CountryService.class, Country.class);
     }
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------------ find
 
     /**
      * Tests {@link CountryService#find(String)} method.
@@ -183,19 +183,17 @@ class CountryServiceIT extends BaseEntityServiceIT<CountryService, Country> {
     }
 
     /**
-     * Asserts {@link CountryService#find(String)} method an empty optional for an unknown country.
+     * Asserts {@link CountryService#find(String)} method returns an empty optional for an unknown country.
      */
     // TODO: 09/09/2019 enable, assert fails, implement, and assert passes.
     @Disabled
     @Test
     void assertFindReturnsEmptyForUnknownCountry() {
         final Optional<Country> found = serviceInstance().find("Machu Picchu");
-        assertThat(found)
-                .isEmpty()
-        ;
+        assertThat(found).isEmpty();
     }
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------------ list
 
     /**
      * Tests {@link CountryService#list(Integer, Integer)} method.
